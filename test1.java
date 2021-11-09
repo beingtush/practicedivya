7be4464594f1b2ac9f693a5de29ca5d0c373d92c
Tushar Raj <Beingtush@gmail.com>
2021-11-09 23:30:43 +0530

    Made some changes in test1.java

--- test1.java
+++ test1.java
@@ -1,6 +1,8 @@
 class test1{
     public static void main(String[] args) {
-        System.out.println("Test executed successfully");    
+        System.out.println("Test run successfully");
+
+        System.out.println("New line added");
     }
     
 }
